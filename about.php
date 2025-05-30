a49da26a7da8130ff90e2ab7c05680fb3c75dea4
Walter Developer <47099208+WalterDeveloper@users.noreply.github.com>
2025-05-30 10:53:27 +0200

    Update about.php

--- about.php
+++ about.php
@@ -10,10 +10,6 @@ foreach ($result as $row) {
     $about_banner = $row['about_banner'];
 }
 ?>
-
-
-
-
 <div class="page-banner" style="background-image: url(assets/uploads/<?php echo $about_banner; ?>);">
     <div class="inner">
         <h1><?php echo $about_title; ?></h1>
